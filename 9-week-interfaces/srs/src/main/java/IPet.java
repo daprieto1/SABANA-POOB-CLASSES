9e3922f5d836f31ff060d2f761e15fe317539a63
Diego Prieto <diego.prieto@pager.com>
2021-03-26 17:30:21 -0500

    fix: add interfaces

--- 9-week-interfaces/srs/src/main/java/IPet.java
+++ 9-week-interfaces/srs/src/main/java/IPet.java
@@ -1,12 +1,14 @@
 public interface IPet {
 
+    boolean EAT = true;
+
     boolean roll();
 
     String sayLoveYourMaster(String masterName);
 
     default boolean eat() {
         System.out.println("Off course I'm gonna eat, it is my unique job here.");
-        return true;
+        return IPet.EAT;
     }
 
 }
